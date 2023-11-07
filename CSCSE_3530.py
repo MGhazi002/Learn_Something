076926f3e216a1a71d6c1e20705509b0761bf0be
mghazi002 <muhammadghazi@my.unt.edu>
2023-11-06 18:43:59 -0600

    Muhammad Ghazi - Ifttt - 11/06

--- CSCSE_3530.py
+++ CSCSE_3530.py
@@ -1,1 +1,16 @@
-print("Hello This is an IFTT Test")
+import random
+
+# List of first names
+first_names = ['Happy', 'Sleepy', 'Bashful', 'Sneezy', 'Grumpy', 'Dopey', 'Doc']
+
+# List of last names
+last_names = ['Apple', 'Orange', 'Banana', 'Cherry', 'Berry', 'Peach', 'Plum']
+
+# Randomly choose a first and last name
+random_first = random.choice(first_names)
+random_last = random.choice(last_names)
+
+# Combine them into a full name
+full_name = random_first + " " + random_last
+
+print("Your random name is:", full_name)
